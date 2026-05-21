f74caee763ccb1701017904d0f290289c17787d4
matias-sciarra <Matiassciarra1@gmail.com>
2026-05-21 11:54:48 -0300

    UIMANAGER

--- Assets/Scripts/InteractionArea.cs
+++ Assets/Scripts/InteractionArea.cs
@@ -3,12 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class InteractionArea : MonoBehaviour
+ 
+ {
+    public int score = 0;
+    public UIManager uiManagerScript;
+    
+    void Awake()
+    {
+        uiManagerScript = GameObject.FindObjectOfType<UIManager>();
+    } 
 
-{
     void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.CompareTag("Collectible")){
             Destroy(other.gameObject);
+            score ++;
+            uiManagerScript.UpdateScore(score);
         }
     }
 }
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+    public TextMeshProUGUI textoscore;
+    public TextMeshProUGUI texto;
+
+    void Start()
+    {
+        UpdateScore(0);
+    }
+        public void UpdateScore(int score){
+            textoscore.text = "Score: " + score.ToString();
+        }
+
+}
